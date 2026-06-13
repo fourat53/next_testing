fef2c14880f5186ea5205d3708b7b520ebd1fd8f
fourat53 <fourat610654@gmail.com>
2026-06-13 17:28:51 +0100

    added prisma+postgres dependencies

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactCompiler: true,
   images: {
-    domains: ["media.licdn.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
   },
 };
 
